271fbd117b7e2c01c121f9b4be7b09ca72f62e3b
Mark Roberts <markro@cs.washington.edu>
2017-04-09 13:32:41 -0700

    tools and instructions update

--- collect-inv-numbers.sh
+++ collect-inv-numbers.sh
@@ -1,42 +1,63 @@
- date;./runone.py boofcv > bo.log 2>&1;date
+ date;./runone.py boofcv ;date
+ mv dljc.log bo.log
  ./count-inv.pl bo.log
- date;./runone.py catalano > ca.log 2>&1;date
+ date;./runone.py catalano ;date
+ mv dljc.log ca.log
  ./count-inv.pl ca.log
- date;./runone.py commonsmath > co.log 2>&1;date
+ date;./runone.py commonsmath ;date
+ mv dljc.log co.log
  ./count-inv.pl co.log
- date;./runone.py dyn4j > dy.log 2>&1;date
+ date;./runone.py dyn4j ;date
+ mv dljc.log dy.log
  ./count-inv.pl dy.log
- date;./runone.py ejml > ej.log 2>&1;date
+ date;./runone.py ejml ;date
+ mv dljc.log ej.log
  ./count-inv.pl ej.log
- date;./runone.py facedetection1 > f1.log 2>&1;date
+ date;./runone.py facedetection1 ;date
+ mv dljc.log f1.log
  ./count-inv.pl f1.log
- date;./runone.py facer > fa.log 2>&1;date
+ date;./runone.py facer ;date
+ mv dljc.log fa.log
  ./count-inv.pl fa.log
- date;./runone.py imagej > ij.log 2>&1;date
+ date;./runone.py imagej ;date
+ mv dljc.log ij.log
  ./count-inv.pl ij.log
- date;./runone.py imglib2 > il.log 2>&1;date
+ date;./runone.py imglib2 ;date
+ mv dljc.log il.log
  ./count-inv.pl il.log
- date;./runone.py imgscalr > is.log 2>&1;date
+ date;./runone.py imgscalr ;date
+ mv dljc.log is.log
  ./count-inv.pl is.log
- date;./runone.py jbox2d > jb.log 2>&1;date
+ date;./runone.py jbox2d ;date
+ mv dljc.log jb.log
  ./count-inv.pl jb.log
- date;./runone.py jcodec > jc.log 2>&1;date
+ date;./runone.py jcodec ;date
+ mv dljc.log jc.log
  ./count-inv.pl jc.log
- date;./runone.py jmonkeyengine > jm.log 2>&1;date
+ date;./runone.py jmonkeyengine ;date
+ mv dljc.log jm.log
  ./count-inv.pl jm.log
- date;./runone.py jreactphysics3d > jr.log 2>&1;date
+ date;./runone.py jreactphysics3d ;date
+ mv dljc.log jr.log
  ./count-inv.pl jr.log
- date;./runone.py jviolajones > jv.log 2>&1;date
+ date;./runone.py jviolajones ;date
+ mv dljc.log jv.log
  ./count-inv.pl jv.log
- date;./runone.py ojalgo > oj.log 2>&1;date
+ date;./runone.py ojalgo ;date
+ mv dljc.log oj.log
  ./count-inv.pl oj.log
- date;./runone.py react > re.log 2>&1;date
+ date;./runone.py react ;date
+ mv dljc.log re.log
  ./count-inv.pl re.log
- date;./runone.py Sort07 > s7.log 2>&1;date
+ date;./runone.py Sort07 ;date
+ mv dljc.log s7.log
  ./count-inv.pl s7.log
- date;./runone.py Sort09 > s9.log 2>&1;date
+ date;./runone.py Sort09 ;date
+ mv dljc.log s9.log
  ./count-inv.pl s9.log
- date;./runone.py Sort10 > s1.log 2>&1;date
+ date;./runone.py Sort10 ;date
+ mv dljc.log s1.log
  ./count-inv.pl s1.log
- date;./runone.py thumbnailinator > th.log 2>&1;date
+ date;./runone.py thumbnailinator ;date
+ mv dljc.log th.log
  ./count-inv.pl th.log
